f80764eaadb9e3dbac8a4232b4bc4202aa330e66
pnikulin <pnnikulin@gmail.com>
2023-07-30 20:15:49 +0300

    second commit

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from src.api_connectors import SJ_API_Connector, HH_API_Connector
 
 
 if __name__ == '__main__':
-    superjob = SJ_API_Connector('python')
-    headhunter = HH_API_Connector('python')
+    superjob = SJ_API_Connector()
+    headhunter = HH_API_Connector()
--- src/api_connectors.py
+++ src/api_connectors.py
@@ -4,49 +4,76 @@ import requests
 import datetime
 import time
 from abc import abstractmethod, ABC
+from enum import StrEnum
+
+class Platform(StrEnum):
+    SuperJob ='SuperJob'
+    HeadHunter ='HeadHunter'
+
+class Vacancy:
+    def __init__(self, platform: Platform, name: str, url: str, salary: int, description: str = ''):
+        self.platform = platform
+        self.name = name
+        self.url = url
+        self.salary = salary
+        self.description = description
+
+    def __repr__(self):
+        return f'{self.name} ({self.salary})'
+
+    def __eq__(self, other):
+        return self.salary == other.salary
 
 
-class API_connector(ABC):
+class ApiConnector(ABC):
     @abstractmethod
     def __init__(self):
         pass
 
     @abstractmethod
-    def get_vacancy(self):
+    def get_vacancies(self, keyword):
         pass
 
 
-class SJ_API_Connector(API_connector):
+class SjApiConnector(ApiConnector):
     """класс для подключения к API SuperJob и парсер вакансий"""
 
-    api_key_superjob = os.getenv('SJ_API_KEY')
-    url = 'https://api.superjob.ru/2.0/vacancies/'
-    headers = {'X-Api-App-Id': api_key_superjob}
+    API_KEY = os.getenv('SJ_API_KEY')
+    URL = 'https://api.superjob.ru/2.0/vacancies/'
+    HEADERS = {'X-Api-App-Id': API_KEY}
 
-    def __init__(self, keyword):
-        self.keyword: str = keyword
-        self.payload = {
-            'keyword': self.keyword,
+    def __init__(self):
+        pass
+
+    def get_vacancies(self, keyword):
+        payload = {
+            'keyword': keyword,
             'count': 20,
             'page': 0,
             'archive': False
         }
-
-    def get_vacancy(self):
-        superjob_data = requests.get(self.url, headers=self.headers, params=self.payload).json()
+        superjob_data = requests.get(self.URL, headers=self.HEADERS, params=payload).json()
         super_job_vacancies_list = []
         for vacancy in superjob_data['objects']:
-            keys_vacancy = {
-                'platform': "SuperJob",
-                'profession': vacancy.get('profession'),
-                'salary': vacancy.get('payment_from'),
-                'link': vacancy.get('link'),
-                'currency': vacancy.get("currency")
-            }
-            super_job_vacancies_list.append(keys_vacancy)
+            # keys_vacancy = {
+            #     'platform': "SuperJob",
+            #     'profession': vacancy.get('profession'),
+            #     'salary': vacancy.get('payment_from'),
+            #     'link': vacancy.get('link'),
+            #     'currency': vacancy.get("currency")
+            #}
+            super_job_vacancies_list.append(
+                Vacancy(
+                    platform=Platform.SuperJob,
+                    name=vacancy.get('profession'),
+                    url=vacancy.get('link'),
+                    salary=vacancy.get('payment_from'),
+                )
+            )
         return super_job_vacancies_list
 
-class HH_API_Connector(API_connector):
+
+class HhApiConnector(ApiConnector):
     """класс для подключения к API HH и парсер вакансий"""
     def __init__(self, keyword):
         self.keyword: str = keyword
@@ -59,7 +86,7 @@ class HH_API_Connector(API_connector):
             "archived": False
         }
 
-    def get_vacancy(self):
+    def get_vacancies(self, keyword):
         hh_vacancies_list = []
         hh_data = requests.get(self.api_url_hh, headers=self.header, params=self.payload).json()
         for vacancy in hh_data['items']:
@@ -83,12 +110,15 @@ class HH_API_Connector(API_connector):
         return hh_vacancies_list
 
 
-testsj = SJ_API_Connector('python')
-testsj2 = SJ_API_Connector.get_vacancy(testsj)
+sjapi = SjApiConnector()
+result = sjapi.get_vacancies('Python')
+print(result[0] == result[3])
+print(id(result[0]))
+print(id(result[3]))
 
-testhh = HH_API_Connector('python')
-testhh2 = HH_API_Connector.get_vacancy(testhh)
+# testhh = HH_API_Connector('python')
+# testhh2 = HH_API_Connector.get_vacancy(testhh)
 
 
-print(testsj2)
-print(testhh2)
+print(result)
+#print(testhh2)
